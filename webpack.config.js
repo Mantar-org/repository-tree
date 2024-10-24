b91616db09c507f856016cea37b11823e5f81591
Goku <63316492+UltraInstinct0x@users.noreply.github.com>
2024-10-24 13:10:39 +0300

    legacy deployment

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/repository-tree/'  // Replace with your repository name
-    : '/dist',
+      ? '/repository-tree/'  // This matches your repository name
+      : '/dist/',
     filename: 'repository-tree.js'
   },
   resolveLoader: {
@@ -55,7 +55,6 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
